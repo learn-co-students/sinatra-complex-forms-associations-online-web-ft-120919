a5148c6091e5b826ee1b063db48d15b3a249d1e3
mrarthurwhite <mister.arthur.white@gmail.com>
2020-02-13 00:49:18 -0500

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -5,16 +5,26 @@ class PetsController < ApplicationController
     erb :'/pets/index' 
   end
 
-  get '/pets/new' do 
+  get '/pets/new' do
+    @owners = Owner.all
     erb :'/pets/new'
   end
 
-  post '/pets' do 
-
+  post '/pets' do
+    @pet = Pet.create(:name=>params["pet_name"])
+    if !params["owner_id"].empty?
+      @pet.owner = Owner.find(params["owner_id"])
+    end
+    binding.pry
+    if !params["owner_name"].empty?
+      @pet.owner = Owner.create(:name=>params["owner_name"])
+    end
+    @pet.save
     redirect to "pets/#{@pet.id}"
   end
 
-  get '/pets/:id' do 
+  get '/pets/:id' do
+    @owners = Owner.all
     @pet = Pet.find(params[:id])
     erb :'/pets/show'
   end
